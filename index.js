d21cbfded6882c11eea8fbae7709efad15a50546
SURYAKANTSHARMA <suryakantsharma84@gmail.com>
2024-02-04 12:47:09 +0530

    Add schema and mongo db connection

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser');
+require('dotenv').config();
+
 
 const password = process.env.PASSWORD;
 const email = process.env.EMAIL;
 
 // Connect to the database
-mongoose.connect('mongodb+srv://${email}:${password}@cluster0.mauoshz.mongodb.net/', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+mongoose.connect('mongodb+srv://${email}:${password}@cluster0.mauoshz.mongodb.net/?retryWrites=true&w=majority')
 
 // Check if the connection is successful
 const db = mongoose.connection
@@ -17,6 +16,32 @@ db.on('error', console.error.bind(console, 'connection error:'))
 db.once('open', function() {
   console.log('Connected to the database')
 })
+
+const user = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true
+  },  
+  count: {
+    type: Number,
+    default: 0
+  }
+});
+
+const exercise = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  description: String, 
+  duration: Number,
+  date: Date
+});
+
+const User = mongoose.model('user', user);
+const Excercise = mongoose.model('exercise', exercise);
+
 
 // Export the database connection
 module.exports = db
@@ -35,6 +60,18 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+app.post('/api/users', (req, res) => { 
+   const newUser = new User(
+     { username: req.body.username }
+   )
+
+   newUser.save((err, user) => {
+     if (err) {
+      res.status(500).send(errr)
+     }
+      res.status(201).send(user)
+   })
+}); 
 
 
 
