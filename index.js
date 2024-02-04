643da9198a1f995c7e5095ff3be795ec7700ff6a
SURYAKANTSHARMA <suryakantsharma84@gmail.com>
2024-02-04 12:09:59 +0530

    Add basic set up

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser');
+
+const password = process.env.PASSWORD;
+const email = process.env.EMAIL;
+
+// Connect to the database
+mongoose.connect('mongodb+srv://${email}:${password}@cluster0.mauoshz.mongodb.net/', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+// Check if the connection is successful
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', function() {
+  console.log('Connected to the database')
+})
+
+// Export the database connection
+module.exports = db
+
+
 const app = express()
+// Use the body-parser middleware to parse JSON requests
+app.use(bodyParser.json());
+
 const cors = require('cors')
 require('dotenv').config()
 
